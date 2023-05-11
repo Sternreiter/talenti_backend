7c9bc4b8733f22c942f3233ac2293cf590f24881
plazal <suilppm@gmail.com>
2023-05-11 09:26:09 -0400

    Actualizacion enviroment

--- src/database/database.ts
+++ src/database/database.ts
@@ -2,6 +2,6 @@ import { Sequelize } from "sequelize";
 require('dotenv').config();
 
 export const sequelize = new Sequelize(`${process.env.DATABASE_NAME}`, `${process.env.DATABASE_USER}`, `${process.env.DATABASE_PASS}`, {
-    host: 'localhost',
+    host: `${process.env.DATABASE_URL}`,
     dialect: 'postgres'
 })
